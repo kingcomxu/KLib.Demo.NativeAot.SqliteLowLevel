783bc93f362d65f96ac3bf26ca9e64c5356be9f1
kingcomxu <kingcomxu@vip.qq.com>
2024-02-06 15:19:28 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,316 @@
+﻿using Microsoft.Win32.SafeHandles;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Mail;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace TestSqlite;
+
+internal class Program
+{
+    static unsafe void Main(string[] args)
+    { 
+        var ptrV = Sqlite3.sqlite3_libversion();
+        var str = Marshal.PtrToStringUTF8(ptrV);
+        Console.WriteLine($"Sqlite Version:{str}");
+
+
+        void* pDb;
+
+        int result = 0;
+        var ptrFileName = Marshal.StringToCoTaskMemUTF8("test.db");
+        result = Sqlite3.sqlite3_open((void*)ptrFileName, &pDb);
+        Marshal.FreeCoTaskMem(ptrFileName);
+
+        if (result == Sqlite3.SQLITE_OK)
+        {
+            Console.WriteLine("opened database successfully!");
+        }
+        else
+        {
+            var ptrErrMsg = Sqlite3.sqlite3_errmsg(pDb);
+            var errMsg = Marshal.PtrToStringUTF8(ptrErrMsg);
+            Console.WriteLine($"open result:0x{result:X},  Can't open database: {errMsg}");
+        }
+
+
+        string sql = "CREATE TABLE COMPANY( ID INT PRIMARY KEY NOT NULL, NAME TEXT    NOT NULL, AGE INT NOT NULL, ADDRESS CHAR(50), SALARY REAL );";
+
+        Console.WriteLine($"CREATE TABLE COMPANY result:{Sqlite3.ExecuteSql(pDb, sql)}");
+
+
+        //insert data
+        sql = "INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) " +
+         "VALUES (1, 'Paul', 32, 'California', 20000.00 ); " +
+         "INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) " +
+         "VALUES (2, 'Allen', 25, 'Texas', 15000.00 ); " +
+         "INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY)" +
+         "VALUES (3, 'Teddy', 23, 'Norway', 20000.00 );" +
+         "INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY)" +
+         "VALUES (4, 'Mark', 25, 'Rich-Mond ', 65000.00 );";
+
+        Console.WriteLine($"INSERT INTO COMPANY result:{Sqlite3.ExecuteSql(pDb, sql)}");
+
+
+        var ptrData = Marshal.StringToCoTaskMemUTF8("select * from Company");
+
+        //select rows by callback
+        sql = "SELECT * from COMPANY";
+        void* pErrMsg;
+        var ptrSql = Marshal.StringToCoTaskMemUTF8(sql);
+        result = Sqlite3.sqlite3_exec(pDb, (void*)ptrSql, (nint)(delegate*<nint, int, byte**, byte**, int>)&callback, (void*)ptrData, &pErrMsg);
+        Marshal.FreeCoTaskMem(ptrSql);
+
+        Marshal.FreeCoTaskMem(ptrData);
+        if (result != Sqlite3.SQLITE_OK)
+        {
+            Console.WriteLine($"Sql Exec Error:{Sqlite3.GetErrMsgFromPtr(pErrMsg)}");
+        }
+        else
+        {
+            Console.WriteLine("Select Rows successfully");
+        }
+
+
+        //get table 
+        sql = "SELECT * from COMPANY";
+        void** pResult;
+        int nRow, nCol;
+        ptrSql = Marshal.StringToCoTaskMemUTF8(sql);
+        result = Sqlite3.sqlite3_get_table(pDb, (void*)ptrSql, &pResult, &nRow, &nCol, &pErrMsg);
+        Marshal.FreeCoTaskMem(ptrSql);
+        if (result != Sqlite3.SQLITE_OK)
+        {
+            Console.WriteLine($"Sql Exec Error:{Sqlite3.GetErrMsgFromPtr(pErrMsg)}");
+        }
+        else
+        {
+            //get data from pResult
+            Console.WriteLine($"rows of data:{nRow}");
+            Console.WriteLine($"cols of data:{nCol}");
+
+            //output the column names
+            for (int i = 0; i < nCol; i++)
+            {
+                var colName = Marshal.PtrToStringUTF8((nint)pResult[i]);
+                Console.Write($"{colName}\t");
+            }
+            Console.WriteLine();
+
+            //output the row data
+            for (int i = 1; i < nCol; i++)
+            {
+                for (int colIndex = 0; colIndex < nCol; colIndex++)
+                {
+                    var colValue = Marshal.PtrToStringUTF8((nint)pResult[i * nCol + colIndex]);
+                    Console.Write($"{colValue}\t");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+
+            Sqlite3.sqlite3_free_table(pResult);
+            Console.WriteLine("get table successfully");
+        }
+
+        //////test blob insert and read
+        sql = "CREATE TABLE EmpPhoto3( ID INT PRIMARY KEY NOT NULL, NAME TEXT, PHOTO BLOB, F FLOAT);";
+        Console.WriteLine($"CREATE TABLE EmpPhoto result:{Sqlite3.ExecuteSql(pDb, sql)}");
+
+        //blob insert
+        sql = "INSERT INTO EmpPhoto3(ID,NAME, PHOTO, F)values(?, ?, ?,?);";
+        ptrSql = Marshal.StringToCoTaskMemUTF8(sql);
+        void* stmt;
+        result = Sqlite3.sqlite3_prepare(pDb, (void*)ptrSql, -1/*ptrsql length in byte*/, &stmt, (void**)0);
+        Console.WriteLine($"sqlite3_prepare result:{result}, result == sqlite_OK: {result == Sqlite3.SQLITE_OK}");
+
+        List<string> fileNames = [Path.Combine(AppContext.BaseDirectory, "a.png"), Path.Combine(AppContext.BaseDirectory, "b.ico")];
+
+        for (int i = 0; i < fileNames.Count; i++)
+        {
+
+            //bind Id
+            result = Sqlite3.sqlite3_bind_int(stmt, 1, i);
+            Console.WriteLine($"sqlite3_bind_int result:{result}, result == sqlite_OK: {result == Sqlite3.SQLITE_OK}");
+            if (result != Sqlite3.SQLITE_OK)
+            {
+                var ptrErrMsg = Sqlite3.sqlite3_errmsg(pDb);
+                Console.WriteLine($"sqlite3_step Error:{Marshal.PtrToStringUTF8(ptrErrMsg)}");
+            }
+
+            //bind Name
+            string name = $"name-{i}";
+            var ptrName = Marshal.StringToCoTaskMemUTF8(name);
+            result = Sqlite3.sqlite3_bind_text(stmt, 2, (void*)ptrName, -1, (void*)0);
+            Marshal.FreeCoTaskMem(ptrName);
+            Console.WriteLine($"sqlite3_bind_text result:{result}, result == sqlite_OK: {result == Sqlite3.SQLITE_OK}");
+            if (result != Sqlite3.SQLITE_OK)
+            {
+                var ptrErrMsg = Sqlite3.sqlite3_errmsg(pDb);
+                Console.WriteLine($"sqlite3_step Error:{Marshal.PtrToStringUTF8(ptrErrMsg)}");
+            }
+            //bind blob
+
+            var fileBytes = File.ReadAllBytes(fileNames[i]);
+            var gchFileBytes = GCHandle.Alloc(fileBytes, GCHandleType.Pinned);
+
+            result = Sqlite3.sqlite3_bind_blob(stmt, 3, (void*)gchFileBytes.AddrOfPinnedObject(), fileBytes.Length, (void*)0);
+            Console.WriteLine($"sqlite3_bind_blob result:{result}, result == sqlite_OK: {result == Sqlite3.SQLITE_OK}");
+            gchFileBytes.Free();
+            if (result != Sqlite3.SQLITE_OK)
+            {
+                var ptrErrMsg = Sqlite3.sqlite3_errmsg(pDb);
+                Console.WriteLine($"sqlite3_step Error:{Marshal.PtrToStringUTF8(ptrErrMsg)}");
+            }
+
+
+            //bind f Float 
+
+            var doubleValue = double.Parse($"{i}.{i}") + 55.1234;
+            Console.WriteLine($"doubleValue:{doubleValue}");
+
+            result = Sqlite3.sqlite3_bind_double(stmt, 4, doubleValue);
+            Console.WriteLine($"sqlite3_bind_double result:{result}, result == sqlite_OK: {result == Sqlite3.SQLITE_OK}");
+            if (result != Sqlite3.SQLITE_OK)
+            {
+                var ptrErrMsg = Sqlite3.sqlite3_errmsg(pDb);
+                Console.WriteLine($"sqlite3_step Error:{Marshal.PtrToStringUTF8(ptrErrMsg)}");
+            }
+
+            //step 
+
+            result = Sqlite3.sqlite3_step(stmt);
+            Console.WriteLine($"sqlite3_step result:{result}, result == SQLITE_DONE: {result == Sqlite3.SQLITE_DONE}");
+            if (result == Sqlite3.SQLITE_ERROR)
+            {
+                var ptrErrMsg = Sqlite3.sqlite3_errmsg(pDb);
+                Console.WriteLine($"sqlite3_step Error:{Marshal.PtrToStringUTF8(ptrErrMsg)}");
+            }
+
+            Console.WriteLine($"before reset,  valueof stmt:0x{(nint)stmt:X}");
+            Console.WriteLine($"before reset,  valueof *stmt:0x{*(nint*)stmt:X}");
+            result = Sqlite3.sqlite3_reset(stmt);
+            Console.WriteLine($"sqlite3_reset result:{result}, result == sqlite_OK: {result == Sqlite3.SQLITE_OK},valueof stmt:0x{(nint)stmt:X}");
+            Console.WriteLine($"sqlite3_reset result:{result}, result == sqlite_OK: {result == Sqlite3.SQLITE_OK},valueof *stmt:0x{*(nint*)stmt:X}");
+
+        }
+
+        result = Sqlite3.sqlite3_finalize(stmt);
+        Console.WriteLine($"sqlite3_finalize result:{result}, result == sqlite_OK: {result == Sqlite3.SQLITE_OK}");
+
+
+        //begin read blob
+        Console.WriteLine("begin read blob!");
+
+
+        sql = "select * from empphoto3";
+        ptrSql = Marshal.StringToCoTaskMemUTF8(sql);
+        result = Sqlite3.sqlite3_prepare(pDb, (void*)ptrSql, -1/*ptrsql length in byte*/, &stmt, (void**)0);
+        Marshal.FreeCoTaskMem(ptrSql);
+        Console.WriteLine($"sqlite3_prepare result:{result}, result == sqlite_OK: {result == Sqlite3.SQLITE_OK}");
+        if (result != Sqlite3.SQLITE_OK)
+        {
+            var ptrErrMsg = Sqlite3.sqlite3_errmsg(pDb);
+            Console.WriteLine($"sqlite3_prepare Error:{Marshal.PtrToStringUTF8(ptrErrMsg)}");
+        }
+        result = Sqlite3.sqlite3_step(stmt);
+        Console.WriteLine($"sqlite3_step result:{result}, result == SQLITE_DONE: {result == Sqlite3.SQLITE_DONE}");
+        if (result == Sqlite3.SQLITE_ERROR)
+        {
+            var ptrErrMsg = Sqlite3.sqlite3_errmsg(pDb);
+            Console.WriteLine($"sqlite3_step Error:{Marshal.PtrToStringUTF8(ptrErrMsg)}");
+        }
+
+        while (result == Sqlite3.SQLITE_ROW)
+        { 
+            //col 0 
+            int id = Sqlite3.sqlite3_column_int(stmt, 0);
+            Console.WriteLine(id);
+
+
+            var ptrId = Sqlite3.sqlite3_column_blob(stmt, 0);
+            var blobId = Marshal.PtrToStringUTF8((nint)ptrId);
+
+            Console.WriteLine($"id from blod:{blobId}");
+
+            //col 1
+            var ptrStr = Sqlite3.sqlite3_column_text(stmt, 1);
+            var name = Marshal.PtrToStringUTF8((nint)ptrStr);
+            Console.WriteLine(name);
+
+            var ptrStrBlob = Sqlite3.sqlite3_column_blob(stmt, 1);
+            var nameBlob = Marshal.PtrToStringUTF8((nint)ptrStrBlob);
+            Console.WriteLine($"name from blob:{nameBlob}");
+
+
+
+
+            //col 2 blob 
+            var ptrBlob = Sqlite3.sqlite3_column_blob(stmt, 2);
+            var len = Sqlite3.sqlite3_column_bytes(stmt, 2);
+            var spanBytes = new ReadOnlySpan<byte>(ptrBlob, len);
+            var outputFilename = Path.Combine(AppContext.BaseDirectory, $"read-blob-result-{name}.png");
+            using SafeFileHandle handle = File.OpenHandle(outputFilename, FileMode.Create, FileAccess.Write, FileShare.ReadWrite);
+            RandomAccess.Write(handle, spanBytes, 0);
+            Console.WriteLine($"blob.len:{len}, outputas:{outputFilename}");
+
+
+            //col 3 float
+            var ptrCol4Name = Sqlite3.sqlite3_column_name(stmt, 3);
+            var col4Name = Marshal.PtrToStringUTF8((nint)ptrCol4Name);
+
+            var fValue = Sqlite3.sqlite3_column_double(stmt, 3);
+            Console.WriteLine($"sqlite3_column_double {col4Name}:{fValue}");
+
+            //col 3 float by blob
+            var ptrFBlob = Sqlite3.sqlite3_column_blob(stmt, 3);
+            var blobF = Marshal.PtrToStringUTF8((nint)ptrFBlob);
+
+            Console.WriteLine($"F from blod:{blobF}");
+
+
+
+            //get next row data
+            result = Sqlite3.sqlite3_step(stmt);
+
+        }
+
+        Sqlite3.sqlite3_finalize(stmt);
+
+
+        //endof read blob
+
+        Sqlite3.sqlite3_close(pDb);
+
+
+        Console.ReadKey();
+    }
+
+    static unsafe int callback(nint ptrData, int colCount, byte** colValues, byte** colNames)
+    {
+        if (ptrData != 0)
+        {
+            string? data = Marshal.PtrToStringUTF8((nint)ptrData);
+            Console.WriteLine($"custom data:{data}");
+        }
+
+        for (int i = 0; i < colCount; i++)
+        {
+            void* pColName = colNames[i];
+            string? colName = Marshal.PtrToStringUTF8((nint)pColName);
+
+            void* pColValue = colValues[i];
+            string? colValue = Marshal.PtrToStringUTF8((nint)pColValue);
+
+            Console.WriteLine($"{colName}:{colValue}");
+        }
+
+        Console.WriteLine("========================================================");
+
+
+        return 0;
+    }
+}
---
+++ Sqlite3.cs
@@ -0,0 +1,234 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestSqlite;
+public unsafe class Sqlite3
+{
+    public const int SQLITE_OK = 0;         /* Successful result */
+    /* beginning-of-error-codes */
+    public const int SQLITE_ERROR = 1;       /* Generic error */
+    public const int SQLITE_INTERNAL = 2;       /* Internal logic error in SQLite */
+    public const int SQLITE_PERM = 3;       /* Access permission denied */
+    public const int SQLITE_ABORT = 4;       /* Callback routine requested an abort */
+    public const int SQLITE_BUSY = 5;       /* The database file is locked */
+    public const int SQLITE_LOCKED = 6;       /* A table in the database is locked */
+    public const int SQLITE_NOMEM = 7;       /* A malloc() failed */
+    public const int SQLITE_READONLY = 8;       /* Attempt to write a readonly database */
+    public const int SQLITE_INTERRUPT = 9;       /* Operation terminated by sqlite3_interrupt()*/
+    public const int SQLITE_IOERR = 10;       /* Some kind of disk I/O error occurred */
+    public const int SQLITE_CORRUPT = 11;       /* The database disk image is malformed */
+    public const int SQLITE_NOTFOUND = 12;       /* Unknown opcode in sqlite3_file_control() */
+    public const int SQLITE_FULL = 13;       /* Insertion failed because database is full */
+    public const int SQLITE_CANTOPEN = 14;       /* Unable to open the database file */
+    public const int SQLITE_PROTOCOL = 15;       /* Database lock protocol error */
+    public const int SQLITE_EMPTY = 16;       /* Internal use only */
+    public const int SQLITE_SCHEMA = 17;       /* The database schema changed */
+    public const int SQLITE_TOOBIG = 18;       /* String or BLOB exceeds size limit */
+    public const int SQLITE_CONSTRAINT = 19;       /* Abort due to constraint violation */
+    public const int SQLITE_MISMATCH = 20;       /* Data type mismatch */
+    public const int SQLITE_MISUSE = 21;       /* Library used incorrectly */
+    public const int SQLITE_NOLFS = 22;       /* Uses OS features not supported on host */
+    public const int SQLITE_AUTH = 23;       /* Authorization denied */
+    public const int SQLITE_FORMAT = 24;       /* Not used */
+    public const int SQLITE_RANGE = 25;       /* 2nd parameter to sqlite3_bind out of range */
+    public const int SQLITE_NOTADB = 26;       /* File opened that is not a database file */
+    public const int SQLITE_NOTICE = 27;       /* Notifications from sqlite3_log() */
+    public const int SQLITE_WARNING = 28;       /* Warnings from sqlite3_log() */
+    public const int SQLITE_ROW = 100;       /* sqlite3_step() has another row ready */
+    public const int SQLITE_DONE = 101;       /* sqlite3_step() has finished executing */
+    /* end-of-error-codes */
+    public const int SQLITE_INTEGER = 1;
+    public const int SQLITE_FLOAT = 2;
+    public const int SQLITE_BLOB = 4;
+    public const int SQLITE_NULL = 5;
+    public const int SQLITE_TEXT = 3;
+
+     
+
+    //SQLITE_API const char* sqlite3_libversion(void);
+
+    [DllImport("SQLite3")]
+    public static extern nint sqlite3_libversion();
+
+    //    SQLITE_API int sqlite3_open(
+    //  const char* filename,   /* Database filename (UTF-8) */
+    //  sqlite3 **ppDb          /* OUT: SQLite db handle */
+    //); 
+    [DllImport("SQLite3")]
+    public static extern int sqlite3_open(void* fileName, void** ppDb);
+
+
+
+
+    // typedef int (* sqlite3_callback) (void*, int, char**, char**);
+
+
+    //    SQLITE_API int sqlite3_exec(
+    //  sqlite3*,                                  /* An open database */
+    //  const char* sql,                           /* SQL to be evaluated */
+    //  int (* callback) (void*, int, char**, char**),  /* Callback function */
+    //  void*,                                    /* 1st argument to callback */
+    //  char** errmsg                              /* Error msg written here */
+    //);
+
+    [DllImport("SQLite3")]
+    public static unsafe extern int sqlite3_exec(void* pDb, void* sql, nint callback, void* argToCallback, void** pzErrMsg);
+
+    //SQLITE_API int sqlite3_close(sqlite3*); 
+    [DllImport("SQLite3")]
+    public static extern int sqlite3_close(void* pDb);
+
+
+
+    //SQLITE_API const char* sqlite3_errmsg(sqlite3 *);
+    [DllImport("SQLite3")]
+    public static extern nint sqlite3_errmsg(void* pDb);
+
+    //SQLITE_API void sqlite3_free(void*);
+    [DllImport("SQLite3")]
+    public static unsafe extern nint sqlite3_free(void* ptr);
+
+
+
+
+
+    //    int sqlite3_get_table(
+    //  sqlite3* db,          /* An open database */
+    //  const char* zSql,     /* SQL to be evaluated */
+    //  char*** pazResult,    /* Results of the query */
+    //  int* pnRow,           /* Number of result rows written here */
+    //  int* pnColumn,        /* Number of result columns written here */
+    //  char** pzErrmsg       /* Error msg written here */
+    //);
+    [DllImport("SQLite3")]
+    public static unsafe extern int sqlite3_get_table(void* pDb, void* sql, void*** pazResult, int* pnRow, int* pnColumn, void** pzErrMsg);
+
+
+    //    void sqlite3_free_table(char** result);
+    [DllImport("SQLite3")]
+    public static unsafe extern void sqlite3_free_table(void** result);
+
+    //    //int sqlite3_prepare(
+    //    sqlite3* db,            /* Database handle */
+    //  const char* zSql,       /* SQL statement, UTF-8 encoded */
+    //  int nByte,              /* Maximum length of zSql in bytes. */
+    //  sqlite3_stmt **ppStmt,  /* OUT: Statement handle */
+    //  const char** pzTail     /* OUT: Pointer to unused portion of zSql */
+    //);
+    [DllImport("SQLite3")]
+    public static unsafe extern int sqlite3_prepare(void* pDb, void* sql, int nByte, void** ppStmt, void** pzTail);
+
+    //int sqlite3_bind_blob(sqlite3_stmt*, int, const void*, int n, void (*) (void*));
+    [DllImport("SQLite3")]
+    public static unsafe extern int sqlite3_bind_blob(void* pStmt, int sqlParameterIndex, void* pData, int nByteLen, void* callback);
+
+
+
+
+    //int sqlite3_bind_double(sqlite3_stmt*, int, double);
+    [DllImport("SQLite3")]
+    public static unsafe extern int sqlite3_bind_double(void* pStmt, int sqlParameterIndex, double doubleValue);
+
+    //int sqlite3_bind_int(sqlite3_stmt*, int, int);
+    [DllImport("SQLite3")]
+    public static unsafe extern int sqlite3_bind_int(void* pStmt, int sqlParameterIndex, int intValue);
+    //int sqlite3_bind_int64(sqlite3_stmt*, int, sqlite3_int64);
+    [DllImport("SQLite3")]
+    public static unsafe extern int sqlite3_bind_int64(void* pStmt, int sqlParameterIndex, Int64 int64Value);
+    //int sqlite3_bind_null(sqlite3_stmt*, int);
+    [DllImport("SQLite3")]
+    public static unsafe extern int sqlite3_bind_null(void* pStmt, int sqlParameterIndex);
+    //int sqlite3_bind_text(sqlite3_stmt*, int,const char*,int,void (*) (void*));
+    [DllImport("SQLite3")]
+    public static unsafe extern int sqlite3_bind_text(void* pStmt, int sqlParameterIndex, void* ptrUtf8String, int nByteLen, void* callback);
+
+
+
+
+
+
+
+    //int sqlite3_step(sqlite3_stmt*);
+    [DllImport("SQLite3")]
+    public static unsafe extern int sqlite3_step(void* pStmt);
+    //int sqlite3_finalize(sqlite3_stmt* pStmt);
+    [DllImport("SQLite3")]
+    public static unsafe extern int sqlite3_finalize(void* pStmt);
+
+    //int sqlite3_reset(sqlite3_stmt* pStmt);
+    [DllImport("SQLite3")]
+    public static unsafe extern int sqlite3_reset(void* pStmt);
+
+
+    //const void* sqlite3_column_blob(sqlite3_stmt *, int iCol);
+    [DllImport("SQLite3")]
+    public static unsafe extern void* sqlite3_column_blob(void* pStmt, int iCol);
+
+    //double sqlite3_column_double(sqlite3_stmt*, int iCol);
+    [DllImport("SQLite3")]
+    public static unsafe extern double sqlite3_column_double(void* pStmt, int iCol);
+
+    //int sqlite3_column_int(sqlite3_stmt*, int iCol);
+    [DllImport("SQLite3")]
+    public static unsafe extern int sqlite3_column_int(void* pStmt, int iCol);
+    //sqlite3_int64 sqlite3_column_int64(sqlite3_stmt*, int iCol);
+    [DllImport("SQLite3")]
+    public static unsafe extern Int64 sqlite3_column_int64(void* pStmt, int iCol);
+    //const unsigned char* sqlite3_column_text(sqlite3_stmt*, int iCol);
+    [DllImport("SQLite3")]
+    public static unsafe extern void* sqlite3_column_text(void* pStmt, int iCol);
+
+
+    //const void* sqlite3_column_text16(sqlite3_stmt *, int iCol);
+    [DllImport("SQLite3")]
+    public static unsafe extern void* sqlite3_column_text16(void* pStmt, int iCol);
+
+
+    //sqlite3_value* sqlite3_column_value(sqlite3_stmt*, int iCol);
+    [DllImport("SQLite3")]
+    public static unsafe extern void* sqlite3_column_value(void* pStmt, int iCol);
+
+    //int sqlite3_column_bytes(sqlite3_stmt*, int iCol);
+    [DllImport("SQLite3")]
+    public static unsafe extern int sqlite3_column_bytes(void* pStmt, int iCol);
+
+    //int sqlite3_column_bytes16(sqlite3_stmt*, int iCol);
+    [DllImport("SQLite3")]
+    public static unsafe extern int sqlite3_column_bytes16(void* pStmt, int iCol);
+
+    //int sqlite3_column_type(sqlite3_stmt*, int iCol);
+    [DllImport("SQLite3")]
+    public static unsafe extern int sqlite3_column_type(void* pStmt, int iCol);
+
+    //const char* sqlite3_column_name(sqlite3_stmt *, int N);
+    [DllImport("SQLite3")]
+    public static unsafe extern void* sqlite3_column_name(void* pStmt, int iCol);
+
+    public static string? GetErrMsgFromPtr(void* pzErrMsg)
+    {
+        var errMsg = Marshal.PtrToStringUTF8((nint)pzErrMsg);
+        Sqlite3.sqlite3_free(pzErrMsg); 
+        return errMsg;
+    }
+
+    public static string? ExecuteSql(void* pDb, string sql)
+    {
+        string? retMsg = "OK";
+        var ptrSql = Marshal.StringToCoTaskMemUTF8(sql);
+        void* pErrMsg;
+
+        var result = Sqlite3.sqlite3_exec(pDb, (void**)ptrSql, 0, (void*)0, &pErrMsg);
+        Marshal.FreeCoTaskMem(ptrSql);
+        if (result != Sqlite3.SQLITE_OK)
+        {
+            retMsg = Sqlite3.GetErrMsgFromPtr(pErrMsg);
+        }
+        return retMsg;
+    }
+
+}
